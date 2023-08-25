2289a0e05909a64b9607afd0d0515630795289ed
Debacle2 <143205054+Debacle2@users.noreply.github.com>
2023-08-25 09:30:17 -0700

    Update ExamplePython.py
    Added Job list, Variable No. 2

--- ExamplePython.py
+++ ExamplePython.py
@@ -9,3 +9,8 @@ y = "String 4"
 print (f"x = {x} and y = {y}")
 
 print ("c = {c} and d = {d}".format(c=1, d=2))
+
+v2 = {'John': 'Doctor', 'Jane': 'California Girl', 'Jim': 'Classified'}
+
+for v7, v8 in v2.items():
+    print (f"{v7} is a {v8}")
