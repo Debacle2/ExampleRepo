8a75074040801f8504d8c984cdcf304548c5d521
Debacle2 <143205054+Debacle2@users.noreply.github.com>
2023-08-28 09:30:55 -0700

    Update ExamplePython.py
    Testing Password

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,3 +1,6 @@
+def c1(c):
+    print(c)
+
 a = "String 1"
 b = "String 2"
 
@@ -14,3 +17,6 @@ v2 = {'John': 'Doctor', 'Jane': 'California Girl', 'Jim': 'Classified'}
 
 for v7, v8 in v2.items():
     print (f"{v7} is a {v8}")
+
+c2 = 'Charlie'
+c1(c2)
